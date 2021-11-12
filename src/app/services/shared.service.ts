313b9f7152ff320359c3560a09a43906c4d3883e
Liviu <liviu@luxon-tech.com>
2021-11-12 10:42:11 +0200

    shared service

---
+++ src/app/services/shared.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  private country = new BehaviorSubject<any>({});
+  public contryObs = this.country.asObservable();
+  constructor() { }
+
+  updateContry(selectedCountry:any){
+    this.country.next(selectedCountry);
+  }
+}
